21a7d56cdcab1ccb18fd5ac28026b7e7f441a216
Paranyu KITTINAVAKIT <paranyu.lion@gmail.com>
2024-11-06 10:32:56 +0700

    Add test cases

--- data_processing.py
+++ data_processing.py
@@ -47,3 +47,42 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
+table_db = TableDB()
+cities_table = Table("Cities", "Cities.csv")
+countries_table = Table("Countries", "Countries.csv")
+print(cities_table)
+print(countries_table)
+table_db.insert(cities_table)
+table_db.insert(countries_table)
+
+country_in_EU = countries_table.filter(lambda x: (x['EU'] == 'yes' and x['coastline'] == 'no'))
+# print(country_in_EU)
+list_country_in_EU = []
+for i in range(len(country_in_EU)):
+    list_country_in_EU.append(country_in_EU[i]['country'])
+# print(list_country_in_EU)
+city_in_EU = cities_table.filter(lambda x: x['country'] in list_country_in_EU)
+# print(type(city_in_EU))
+# print(city_in_EU)
+
+# Print the min temperatures for cities in EU that do not have coastlines
+min_temp = (cities_table.aggregate(lambda x: min(x), 'temperature', city_in_EU))
+print("min temperatures for cities in EU that do not have coastlines :")
+print(min_temp)
+print()
+# Print the max temperatures for cities in EU that do not have coastlines
+min_temp = (cities_table.aggregate(lambda x: max(x), 'temperature', city_in_EU))
+print("max temperatures for cities in EU that do not have coastlines :")
+print(min_temp)
+print()
+
+# Print the min latitude for cities in every country
+min_temp = cities_table.aggregate(lambda x: min(x), 'latitude')
+print("min latitude for cities in every country:")
+print(min_temp)
+print()
+# Print the max latitude for cities in every country
+max_temp = cities_table.aggregate(lambda x: max(x), 'latitude')
+print("max latitude for cities in every country:")
+print(max_temp)
+print()
